511cd2eee5fc28c1989797a0b67f1e0566ec2f26
poliakovak <kseniiapoliakova01@gmail.com>
2021-11-21 17:39:21 +0200

    Added view for Employee

---
+++ src/views/EmployeeView.java
@@ -0,0 +1,50 @@
+package views;
+
+import models.Employee;
+import utils.Validator;
+
+import java.util.Scanner;
+
+public class EmployeeView {
+
+    Employee model;
+    String title;
+    String name;
+    int numberOfHours;
+    double ratePerHour;
+    double revenue;
+    Scanner scanner;
+
+    public EmployeeView(Employee model) {this.model = model; }
+
+    public void getInputs() {
+
+        scanner = new Scanner(System.in);
+
+        title = "Введите имя продавца: ";
+        System.out.print(title);
+        name = Validator.validateName(scanner);
+        model.setName(name);
+
+        title = "Введите количество рабочих часов: ";
+        System.out.print(title);
+        numberOfHours = Validator.validateQuantityInput(scanner);
+        model.setNumberOfHours(numberOfHours);
+
+        title = "Введите ставку за час работы: ";
+        System.out.print(title);
+        ratePerHour = Validator.validatePriceInput(scanner);
+        model.setRatePerHour(ratePerHour);
+
+        title = "Введите сумму выручки: ";
+        System.out.print(title);
+        revenue = Validator.validatePriceInput(scanner);
+        model.setRevenue(revenue);
+
+        scanner.close();
+    }
+
+    public void getOutput(String output) {
+        System.out.println(output);
+    }
+}
