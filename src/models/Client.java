67f8bdb483b0451554159e87bce6235548cf8bb6
poliakovak <kseniiapoliakova01@gmail.com>
2021-11-21 11:24:22 +0200

    Added class Client

--- src/models/Client.java
+++ src/models/Client.java
@@ -1,4 +1,37 @@
 package models;
 
+import base.Discount;
+import base.Payment;
+
 public class Client {
+
+    private String name;
+    private int quantity;
+    private double price;
+    private final static int DISCOUNT_RATE = 15;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
