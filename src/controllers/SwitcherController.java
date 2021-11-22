0f22c4091f24347758835eedce682238a192051f
poliakovak <kseniiapoliakova01@gmail.com>
2021-11-22 17:25:39 +0200

    Added controller for Switcher

--- src/controllers/SwitcherController.java
+++ src/controllers/SwitcherController.java
@@ -1,6 +1,30 @@
 package controllers;
 
+import models.Switcher;
+import views.SwitcherView;
+
 public class SwitcherController {
 
+    Switcher model;
+    SwitcherView view;
 
+    public SwitcherController(Switcher model, SwitcherView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void runApp() {
+
+        view.getInput();
+
+        int chosenController = model.getControllerType();
+
+        switch (chosenController)
+        {
+            case 1 -> model.runClient();
+            case 2 -> model.runEmployee();
+            case 3 -> model.runSupplier();
+            default -> throw new IllegalStateException("Unexpected value: " + chosenController);
+        }
+    }
 }
