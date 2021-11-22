a74734ba8e61a831ab2a50dbc1a9ca3fdd1c84a9
poliakovak <kseniiapoliakova01@gmail.com>
2021-11-22 17:15:52 +0200

    Updated view for Switcher

--- src/views/SwitcherView.java
+++ src/views/SwitcherView.java
@@ -1,32 +1,24 @@
 package views;
 
 import models.Switcher;
+import utils.Validator;
 
 import java.util.Scanner;
 
 public class SwitcherView {
 
     Switcher model;
+    Scanner scanner;
 
     int controllerType;
 
     public SwitcherView(Switcher model) {this.model = model; }
 
     public void getInput() {
-        Scanner console = new Scanner(System.in);
-
+        scanner = new Scanner(System.in);
         System.out.print("Для выбора роли нажмите\n1 - Покупатель\n2 - Продавец\n3 - Поставщик: ");
-        controllerType = console.nextInt();
-
-        switch (controllerType) {
-            case 1 -> System.out.println("Вы выбрали Покупателя");
-            case 2 -> System.out.println("Вы выбрали Продавца");
-            case 3 -> System.out.println("Вы выбрали Поставщика");
-            default -> {
-                System.out.println("Такого варианта нет. Попробуйте ещё раз: ");
-                controllerType = console.nextInt();
-            }
-        }
+        controllerType = Validator.validateTypeOfController(scanner);
+        model.setControllerType(controllerType);
 
         model.setControllerType(controllerType);
 
